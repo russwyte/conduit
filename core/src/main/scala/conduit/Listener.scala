a7d89d1b4859b5c7da15b7a0382dc85f76935da8
Russ White <356303+russwyte@users.noreply.github.com>
2025-03-04 16:33:47 -0600

    make private

--- core/src/main/scala/conduit/Listener.scala
+++ core/src/main/scala/conduit/Listener.scala
@@ -1,13 +1,13 @@
 package conduit
 import zio.*
 
-case class Listener[M, S] private[conduit] (
+private[conduit] case class Listener[M, S] private[conduit] (
     cursor: Lens[M, S],
     listener: S => Unit,
 ):
   private var lastValue: Option[S] = None
 
-  def notify(oldModel: M, newModel: M): UIO[Unit] =
+  private[conduit] def notify(oldModel: M, newModel: M): UIO[Unit] =
     val newValue = cursor.get(newModel)
     ZIO.succeed {
       if !lastValue.exists(_ == newValue) then
